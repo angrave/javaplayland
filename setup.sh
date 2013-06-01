ce8e993ac5e03e812f2bead7bcf055ae8ab10c21
Lawrence Angrave <angrave@illinois.eduuutoomanyu>
2013-06-01 12:34:38 -0500

    work

--- setup.sh
+++ setup.sh
@@ -2,14 +2,22 @@
 mkdir -p vendor
 if [ ! -d vendor/doppio ]; then 
 	git clone git://github.com/angrave/doppio.git vendor/doppio
-	pushd
+	pushd .
 	cd vendor/doppio
 	./tools/setup.sh
 	popd
 fi
-cd vendor/doppio
-make library
-cd ../..
+
 mkdir -p web/scripts/doppio/
-cp -r vendor/doppio/build/library/*compressed* web/scripts/doppio/
+mkdir -p web/browser
+DOPPIO=vendor/doppio
+pushd .
+cd $DOPPIO
+make library
+popd
+# Copy doppio JVM
+cp -r $DOPPIO/build/library/*compressed* web/scripts/doppio/
+# listings contains a list of server-side runtime classes available for download
+ln -sfn $(cd $DOPPIO/build/release/browser;pwd)/listings.json web/browser/listings.json
+
 
