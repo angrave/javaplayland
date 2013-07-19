64ee363940141e5a1ac6a7affd3a8121206217ae
Lawrence Angrave <angrave@illinois.eduuutoomanyu>
2013-07-19 12:57:54 -0500

    new bsh support

---
+++ util/compile-bsh-extras.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ ! -d web/doppio-jvm ] ; then
+    echo 'wrong dir'
+    exit 1
+fi
+
+BASE=`pwd`
+TGT=web/doppio-jvm
+DOPPIO=$BASE/vendor/doppio
+COFFEEC=coffee
+
+SOURCEPATH=src/beanshellextras/
+CLASSPATH=$TGT/vengor/classes
+
+# Todo insert your javac here
+# -g:none ; no debug info 
+javac -sourcepath SOURCEPATH -classpath $CLASSPATH -d  $CLASSPATH  Mine.java
+
+#Classes should be relative to $TGT/vengor/classes
+# e.g. bsh.Interpreter.class is at $TGT/vengor/classes/bshInterpreter.class
+
+(cd $TGT; coffee $DOPPIO/tools/gen_dir_listings.coffee > listings.json)
+
