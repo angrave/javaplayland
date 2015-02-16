6266fc6c592f960d3fd628a3047055290f893724
themasterofrobots <mw1720978@yahoo.com>
2015-02-16 00:07:55 -0600

    Braces Fixes

--- web/scripts/gui/gameManager.js
+++ web/scripts/gui/gameManager.js
@@ -40,6 +40,7 @@
       this.commandsValid = __bind(this.commandsValid, this);
       this.startGame = __bind(this.startGame, this);
       this.gameName = __bind(this.gameName, this);
+      this.checkBraces = __bind(this.checkBraces, this);
 
       /*
           External Function (used by something outside of this file)
@@ -458,6 +459,7 @@
       $('#stopRun').show();
       this.environment.stats.runCount += 1;
       this.storeStats();
+      var braces = this.checkBraces(code)
       if (this.environment.backEnd === 'interpreter') {
         this.codeEditor.scan();
         if (!this.canRun) {
@@ -539,46 +541,47 @@
           Used to check for matching braces.
        */
        //Since the code string is pass by value, we can mess with it
-       var offset = 0
+       var offset = 0;
        while(code.length > 0)
        {
-        var lcom = code.indexOf("\\*")
-        var rcom = code.indexOf("*\\")
-        var bcom = code.indexOf("\\\\")
-        var lbrace = code.indexOf("{")
-        var rbrace = code.lastIndexOf("}")
+        var lcom = code.indexOf("\\*");
+        var rcom = code.indexOf("*\\");
+        var bcom = code.indexOf("\\\\");
+        var lbrace = code.indexOf("{");
+        var rbrace = code.lastIndexOf("}");
 
         //We need to determine if the braces are an issue
         //If no braces, then we have a valid program
         if(lbrace == -1 && rbrace == -1)
-          return 0
+          return 0;
         //Otherwise, if we have a comment begin before a brace
         else if(lcom != -1 && lcom < lbrace)
           //If no matching right comment, then we have a comment mismatch
           if(rcom == -1)
-            return offset + lcom
+            return offset + lcom;
           //Otherwise scan again after the comment ends
           else
           {
-            code = code.substr(rcom+2, code.length)
-            offset += rcom+2
-            continue
+            code = code.substr(rcom+2, code.length);
+            offset += rcom+2;
+            continue;
           }
         else if(bcom != -1 && bcom < lbrace)
         {
             //Line comment. Find where it ends, eliminate anything before that
-            code = code.substr(bcom+2, code.length)
-            endOfLine = code.indexOf("\n")
+            code = code.substr(bcom+2, code.length);
+            endOfLine = code.indexOf("\n");
             //If no newline, then we run into the end of the program. We are done with checking matching
             if(endOfLine == -1)
-              return 0
-            code = code.substr(endOfLine+2, temp.length)
-            offset += bcom + endOfLine + 4
+              return 0;
+            code = code.substr(endOfLine+2, temp.length);
+            offset += bcom + endOfLine + 4;
         }
-        else if(lbrace * rbrace < 0) //One of the two is -1
-          return offset + lbrace * rbrace * -1 //This will give the mismatched position
+        else if(lbrace * rbrace < 0); //One of the two is -1
+          return offset + lbrace * rbrace * -1; //This will give the mismatched position
         else
-          code = code.substr(lbrace+1, rbrace)
+          //recurse into an inner brace (or next set of braces)
+          code = code.substr(lbrace+1, rbrace);
        }
 
      }
