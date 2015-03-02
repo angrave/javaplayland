e9276ee3b3a52dad4d61e743d3a6750fc0b22563
hempywh <will.hempy@gmail.com>
2015-03-01 18:59:18 -0600

    Wrote first legitimate test. Also, the file MUST be run from http://localhost/web/test.html otherwise the AJAX request tests WILL FAIL.

--- web/tests/util/utilsTest.js
+++ web/tests/util/utilsTest.js
@@ -1,8 +1,27 @@
 describe("Utils",function() {
 
-  it("Loads the configuration file containing styling", function() {
-	var cssData;
-	expect(findConfig).toEqual(jasmine.any(Function));
-	findConfig('./config/color0050');
-  });
+	describe("findConfig",function() {
+	  it("The utility function is in place from previous javascript", function() {
+		expect(findConfig).toEqual(jasmine.any(Function));
+	  });
+	  
+	  it("The findConfig function is correctly described", function() {
+		console.log(findConfig);
+		expect(findConfig.toString()).toEqual(jasmine.stringMatching("cssData"));
+	  });
+	  
+	  it("Some CSS data is returned from AJAX request\
+			(must host on localhost/web/test.html)", function () {
+		var cssData = findConfig('config/color0050.json');
+		console.log(cssData);
+		expect(cssData).not.toBeNull();
+	  });
+	  
+	  it("Correct color0050.json CSS data is returned (must host on localhost/web/test.html)", function() {
+		var cssData = findConfig('config/color0050.json');
+		console.log(cssData['title']);
+		expect(cssData['title']).toEqual(jasmine.stringMatching("Color by code"));
+		expect(cssData).toEqual(jasmine.objectContaining({title : "Color by code"}));
+	  });
+	});
 });
