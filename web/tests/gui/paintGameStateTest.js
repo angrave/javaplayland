cf53b09c0347a73fa21e5994c224d321ca49e58f
hempywh <will.hempy@gmail.com>
2015-03-16 04:40:52 -0500

    Interconnected files makes testing very complicated. I need more time to investigate this.

--- web/tests/gui/paintGameStateTest.js
+++ web/tests/gui/paintGameStateTest.js
@@ -1,7 +1,21 @@
 describe("paintGameStateTest",function() {
 	
-	it("",function() {
-		//TODO your code here
+	it("game state exists",function() {
+		expect(window.PaintGameState).not.toBeNull();
 	});
 	
+	it("game state has properties",function() {
+		expect(window.PaintGameState.stopGame).not.toBeNull();
+		expect(window.PaintGameState.gameManager).not.toBeNull();
+		expect(window.PaintGameState.gameLost).not.toBeNull();
+		expect(window.PaintGameState.gameWon).not.toBeNull();
+		expect(window.PaintGameState.clock).not.toBeNull();
+	});
+	/**
+	it("getPixel works correctly",function() {
+		paintGameStateTester = PaintGameState(gameManager, waitForCode);
+		alert(paintGameStateTester);
+		expect(window.PaintGameState.prototype.getPixel(0,0)).toEqual(window.PaintGameState.gameState.getPixel(0,0));
+	});
+	**/
 });
