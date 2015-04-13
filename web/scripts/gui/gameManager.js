20321b13872f5be9e063f092d0f88df135200be8
themasterofrobots <mw1720978@yahoo.com>
2015-04-13 17:19:28 -0500

    Few additions to the compile error messages

--- web/scripts/gui/gameManager.js
+++ web/scripts/gui/gameManager.js
@@ -590,13 +590,13 @@
        while(index < maxLen)
        {
         if(rcoms[index] < lcoms[index])
-          return "Mismatched closing comment at " + this.findLineNum(rcoms[index], newLines);
+          return "Mismatched closing comment at line " + this.findLineNum(rcoms[index], newLines) + ". Fix this be either removing it or adding a matching opening comment.";
         index += 1;
        }
        if(lcoms.length > rcoms.length)
-        return "Mismatched opening comment at " + this.findLineNum(lcoms[rcoms.length+1], newLines);
+        return "Mismatched opening comment at line " + this.findLineNum(lcoms[rcoms.length+1], newLines) + ". Fix this be either removing it or adding a matching closing comment.";
        if(rcoms.length > lcoms.length)
-        return "Mismatched closing comment at " + this.findLineNum(rcoms[lcoms.length+1], newLines);
+        return "Mismatched closing comment at line " + this.findLineNum(rcoms[lcoms.length+1], newLines) + ". Fix this be either removing it or adding a matching opening comment.";
 
        //New to check braces. Same as before, but now must ignore braces in comments
        var lindex = 0, rindex = 0;
@@ -609,15 +609,15 @@
         else
         {
           if(rbraces[rindex] < lbraces[lindex])
-            return "Mismatched right brace at " + this.findLineNum(rbraces[rindex], newLines);
+            return "Mismatched right brace at line " + this.findLineNum(rbraces[rindex], newLines) + ". Fix this be either removing it or adding a matching left brace.";
           lindex += 1;
           rindex += 1;
         }
        }
        if(lindex != lbraces.length)
-        return "Mismatched left brace at " + this.findLineNum(lbraces[lindex], newLines);
+        return "Mismatched left brace at line " + this.findLineNum(lbraces[lindex], newLines) + ". Fix this be either removing it or adding a matching right brace";
        if(rindex != rbraces.length)
-        return "Mismatched right brace at " + this.findLineNum(rbraces[rindex], newLines);
+        return "Mismatched right brace at line " + this.findLineNum(rbraces[rindex], newLines) + ". Fix this be either removing it or adding a matching left brace";
 
        return 0;
      }
@@ -646,11 +646,11 @@
           Used to check for matching braces. Finds the line number
        */
        var index = 0;
-       while(lines[startIndex] < position)
+       while(lines[index] < position)
        {
         index += 1;
        }
-       return index+1;
+       return String(index+1);
      }
 
      GameManager.prototype.betweenIndices = function(position, fIndices, sIndices) {
